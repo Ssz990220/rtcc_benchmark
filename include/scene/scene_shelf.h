d98474d0587f89505dd6f31e7fcdf6fd53b4bcb9
sizhe <ssz990220@sjtu.edu.cn>
2024-02-16 22:14:03 +0800

    new scene

---
+++ include/scene/scene_shelf.h
@@ -0,0 +1,209 @@
+#pragma once
+
+#include "scene_utils.h"
+#include <ros/package.h>
+#include <geometric_shapes/mesh_operations.h>
+#include <moveit/planning_scene/planning_scene.h>
+
+void setCollisionScene(std::shared_ptr<planning_scene::PlanningScene> planning_scene)
+{
+    {
+        std::string meshPath = "file://" + ros::package::getPath("rtcc_benchmark") + "/data/assets/bin/bin.obj";
+        Eigen::Isometry3d meshPose = Eigen::Isometry3d::Identity();
+
+        // Set the translation part
+        meshPose.translation() << 0, -0.6, 0;
+
+        add_mesh(planning_scene, meshPath, "bin1", meshPose);
+    }
+
+    {
+        std::string meshPath = "file://" + ros::package::getPath("rtcc_benchmark") + "/data/assets/bin/bin.obj";
+        Eigen::Isometry3d meshPose = Eigen::Isometry3d::Identity();
+
+        // Set the translation part
+        meshPose.translation() << 0, 0.6, 0;
+
+        add_mesh(planning_scene, meshPath, "bin2", meshPose);
+    }
+
+    {
+        std::string meshPath = "file://" + ros::package::getPath("rtcc_benchmark") + "/data/assets/shelves/shelves.obj";
+        Eigen::Isometry3d meshPose = Eigen::Isometry3d::Identity();
+
+        // Set the translation part
+        meshPose.translation() << 0.85, 0, 0.4;
+
+        add_mesh(planning_scene, meshPath, "shelf", meshPose);
+    }
+}
+
+//   Bob:
+//     pose: [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]
+//     file_path: "scene/Bob.obj"
+//   Face1:
+//     pose: [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]
+//     file_path: "scene/Face1.obj"
+//   Face2:
+//     pose: [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]
+//     file_path: "scene/Face2.obj"
+//   Face3:
+//     pose: [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]
+//     file_path: "scene/Face3.obj"
+//   Cow:
+//     pose: [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]
+//     file_path: "scene/Cow.obj"
+//   City:
+//     pose: [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]
+//     file_path: "scene/City.obj"
+//   Bridge:
+//     pose: [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]
+//     file_path: "scene/Bridge.obj"
+//   Fish:
+//     pose: [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]
+//     file_path: "scene/Fish.obj"
+//   Sheep:
+//     pose: [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]
+//     file_path: "scene/Sheep.obj"
+//   Rose:
+//     pose: [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]
+//     file_path: "scene/Rose.obj"
+//   Board:
+//     pose: [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]
+//     file_path: "scene/Snakeboard.obj"
+//   Yeah:
+//     pose: [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]
+//     file_path: "scene/Yeah.obj"
+void setCollisionScene(std::shared_ptr<planning_scene::PlanningScene> planning_scene, visualization_msgs::MarkerArray &g_collision_objs, std::unique_ptr<ros::Publisher>& g_col_marker_publisher)
+{
+    {
+        std::string meshPath = "file://" + ros::package::getPath("rtcc_benchmark") + "/data/assets/bin/bin.obj";
+        Eigen::Isometry3d meshPose = Eigen::Isometry3d::Identity();
+
+        // Set the translation part
+        // meshPose.translation() << 0, -0.6, 0;
+        meshPose.translation() << 0, -0.4, 0;
+
+        add_mesh(planning_scene, meshPath, "bin1", meshPose, g_collision_objs);
+    }
+
+    {
+        std::string meshPath = "file://" + ros::package::getPath("rtcc_benchmark") + "/data/assets/bin/bin.obj";
+        Eigen::Isometry3d meshPose = Eigen::Isometry3d::Identity();
+
+        // Set the translation part
+        // meshPose.translation() << 0, 0.6, 0;
+        meshPose.translation() << 0, 0.4, 0;
+
+        add_mesh(planning_scene, meshPath, "bin2", meshPose, g_collision_objs);
+    }
+
+    {
+        std::string meshPath = "file://" + ros::package::getPath("rtcc_benchmark") + "/data/assets/shelves/shelves.obj";
+        Eigen::Isometry3d meshPose = Eigen::Isometry3d::Identity();
+
+        // Set the translation part
+        // meshPose.translation() << 0.85, 0, 0.4;
+        meshPose.translation() << 0.45, 0, 0.4;
+
+        add_mesh(planning_scene, meshPath, "shelf", meshPose, g_collision_objs);
+    }
+
+    {
+        std::string meshPath = "file://" + ros::package::getPath("rtcc_benchmark") + "/data/assets/Bob.obj";
+        Eigen::Isometry3d meshPose = Eigen::Isometry3d::Identity();
+        meshPose.translation() << -0.4, 0, 0;
+
+        add_mesh(planning_scene, meshPath, "Bob", meshPose, g_collision_objs);
+    }
+
+    {
+        std::string meshPath = "file://" + ros::package::getPath("rtcc_benchmark") + "/data/assets/Face1.obj";
+        Eigen::Isometry3d meshPose = Eigen::Isometry3d::Identity();
+        meshPose.translation() << -0.4, 0, 0;
+
+        add_mesh(planning_scene, meshPath, "Face1", meshPose, g_collision_objs);
+    }
+
+    {
+        std::string meshPath = "file://" + ros::package::getPath("rtcc_benchmark") + "/data/assets/Face2.obj";
+        Eigen::Isometry3d meshPose = Eigen::Isometry3d::Identity();
+        meshPose.translation() << -0.4, 0, 0;
+
+        add_mesh(planning_scene, meshPath, "Face2", meshPose, g_collision_objs);
+    }
+
+    {
+        std::string meshPath = "file://" + ros::package::getPath("rtcc_benchmark") + "/data/assets/Face3.obj";
+        Eigen::Isometry3d meshPose = Eigen::Isometry3d::Identity();
+        meshPose.translation() << -0.4, 0, 0;
+
+        add_mesh(planning_scene, meshPath, "Face3", meshPose, g_collision_objs);
+    }
+
+    {
+        std::string meshPath = "file://" + ros::package::getPath("rtcc_benchmark") + "/data/assets/Cow.obj";
+        Eigen::Isometry3d meshPose = Eigen::Isometry3d::Identity();
+        meshPose.translation() << -0.4, 0, 0;
+
+        add_mesh(planning_scene, meshPath, "Cow", meshPose, g_collision_objs);
+    }
+
+    {
+        std::string meshPath = "file://" + ros::package::getPath("rtcc_benchmark") + "/data/assets/City.obj";
+        Eigen::Isometry3d meshPose = Eigen::Isometry3d::Identity();
+        meshPose.translation() << -0.4, 0, 0;
+
+        add_mesh(planning_scene, meshPath, "City", meshPose, g_collision_objs);
+    }
+
+    {
+        std::string meshPath = "file://" + ros::package::getPath("rtcc_benchmark") + "/data/assets/Bridge.obj";
+        Eigen::Isometry3d meshPose = Eigen::Isometry3d::Identity();
+        meshPose.translation() << -0.4, 0, 0;
+
+        add_mesh(planning_scene, meshPath, "Bridge", meshPose, g_collision_objs);
+    }
+
+    {
+        std::string meshPath = "file://" + ros::package::getPath("rtcc_benchmark") + "/data/assets/Fish.obj";
+        Eigen::Isometry3d meshPose = Eigen::Isometry3d::Identity();
+        meshPose.translation() << -0.4, 0, 0;
+
+        add_mesh(planning_scene, meshPath, "Fish", meshPose, g_collision_objs);
+    }
+
+    {
+        std::string meshPath = "file://" + ros::package::getPath("rtcc_benchmark") + "/data/assets/Sheep.obj";
+        Eigen::Isometry3d meshPose = Eigen::Isometry3d::Identity();
+        meshPose.translation() << -0.4, 0, 0;
+
+        add_mesh(planning_scene, meshPath, "Sheep", meshPose, g_collision_objs);
+    }
+
+    {
+        std::string meshPath = "file://" + ros::package::getPath("rtcc_benchmark") + "/data/assets/Rose.obj";
+        Eigen::Isometry3d meshPose = Eigen::Isometry3d::Identity();
+        meshPose.translation() << -0.4, 0, 0;
+
+        add_mesh(planning_scene, meshPath, "Rose", meshPose, g_collision_objs);
+    }
+
+    {
+        std::string meshPath = "file://" + ros::package::getPath("rtcc_benchmark") + "/data/assets/Snakeboard.obj";
+        Eigen::Isometry3d meshPose = Eigen::Isometry3d::Identity();
+        meshPose.translation() << -0.4, 0, 0;
+
+        add_mesh(planning_scene, meshPath, "Board", meshPose, g_collision_objs);
+    }
+
+    {
+        std::string meshPath = "file://" + ros::package::getPath("rtcc_benchmark") + "/data/assets/Yeah.obj";
+        Eigen::Isometry3d meshPose = Eigen::Isometry3d::Identity();
+        meshPose.translation() << -0.4, 0, 0;
+
+        add_mesh(planning_scene, meshPath, "Yeah", meshPose, g_collision_objs);
+    }
+
+    g_col_marker_publisher->publish(g_collision_objs);
+}
--- include/scene/scene_utils.h
+++ include/scene/scene_utils.h
@@ -1,19 +1,16 @@
 #pragma once
 
-
-#include <geometric_shapes/mesh_operations.h>
 #include <geometric_shapes/mesh_operations.h>
 #include <moveit/planning_scene/planning_scene.h>
+#include <moveit_visual_tools/moveit_visual_tools.h>
 
-
-
-void add_mesh(collision_detection::CollisionEnvPtr planning_scene, std::string path, std::string name, Eigen::Isometry3d &pose)
+void add_mesh(std::shared_ptr<planning_scene::PlanningScene> &planning_scene, std::string path, std::string name, Eigen::Isometry3d &pose)
 {
     auto mesh = std::shared_ptr<shapes::Mesh>(shapes::createMeshFromResource(path));
-    planning_scene->getWorld()->addToObject(name, mesh, pose);
+    planning_scene->getWorldNonConst()->addToObject(name, mesh, pose);
 }
 
-void add_cube(collision_detection::CollisionEnvPtr planning_scene, std::array<double, 6> &cubeInfo, std::string name)
+void add_cube(std::shared_ptr<planning_scene::PlanningScene> &planning_scene, std::array<double, 6> &cubeInfo, std::string name)
 {
 
     // Create a shared pointer to a new box shape
@@ -23,5 +20,74 @@ void add_cube(collision_detection::CollisionEnvPtr planning_scene, std::array<do
     pose.translation() = Eigen::Vector3d(cubeInfo[3], cubeInfo[4], cubeInfo[5]);
 
     // Add the cube to the planning scene
-    planning_scene->getWorld()->addToObject(name, cube, pose);
+    planning_scene->getWorldNonConst()->addToObject(name, cube, pose);
+}
+
+
+void add_mesh(std::shared_ptr<planning_scene::PlanningScene> &planning_scene, std::string path, std::string name, Eigen::Isometry3d &pose, visualization_msgs::MarkerArray &g_collision_objs)
+{
+  auto mesh = std::shared_ptr<shapes::Mesh>(shapes::createMeshFromResource(path));
+  planning_scene->getWorldNonConst()->addToObject(name, mesh, pose);
+
+  visualization_msgs::Marker g_mesh;
+  g_mesh.header.frame_id = "base";
+  g_mesh.header.stamp = ros::Time::now();
+  g_mesh.ns = name;
+  g_mesh.type = visualization_msgs::Marker::MESH_RESOURCE;
+  g_mesh.action = visualization_msgs::Marker::ADD;
+  g_mesh.pose.position.x = pose.translation().x();
+  g_mesh.pose.position.y = pose.translation().y();
+  g_mesh.pose.position.z = pose.translation().z();
+  Eigen::Quaterniond quat(pose.rotation());
+  g_mesh.pose.orientation.x = quat.x();
+  g_mesh.pose.orientation.y = quat.y();
+  g_mesh.pose.orientation.z = quat.z();
+  g_mesh.pose.orientation.w = quat.w();
+  g_mesh.scale.x = 1;
+  g_mesh.scale.y = 1;
+  g_mesh.scale.z = 1;
+  g_mesh.color.a = 0.8; // Don't forget to set the alpha!
+  g_mesh.color.r = 0.0;
+  g_mesh.color.g = 1.0;
+  g_mesh.color.b = 0.0;
+  g_mesh.lifetime = ros::Duration();
+  g_mesh.mesh_resource = path;
+  g_collision_objs.markers.push_back(g_mesh);
+}
+
+void add_cube(std::shared_ptr<planning_scene::PlanningScene> &planning_scene, std::array<double, 6> &cubeInfo, std::string name, visualization_msgs::MarkerArray &g_collision_objs)
+{
+
+  // Create a shared pointer to a new box shape
+  shapes::ShapePtr cube = std::make_shared<shapes::Box>(cubeInfo[0], cubeInfo[1], cubeInfo[2]);
+  // Define the pose of the cube
+  Eigen::Isometry3d pose = Eigen::Isometry3d::Identity();
+  pose.translation() = Eigen::Vector3d(cubeInfo[3], cubeInfo[4], cubeInfo[5]);
+
+  // Add the cube to the planning scene
+  planning_scene->getWorldNonConst()->addToObject(name, cube, pose);
+
+  // Add to marker array
+  visualization_msgs::Marker marker;
+  marker.header.frame_id = "base";
+  marker.header.stamp = ros::Time::now();
+  marker.ns = name;
+  marker.type = visualization_msgs::Marker::CUBE;
+  marker.action = visualization_msgs::Marker::ADD;
+  marker.pose.position.x = cubeInfo[3];
+  marker.pose.position.y = cubeInfo[4];
+  marker.pose.position.z = cubeInfo[5];
+  marker.pose.orientation.x = 0.0;
+  marker.pose.orientation.y = 0.0;
+  marker.pose.orientation.z = 0.0;
+  marker.pose.orientation.w = 1.0;
+  marker.scale.x = cubeInfo[0];
+  marker.scale.y = cubeInfo[1];
+  marker.scale.z = cubeInfo[2];
+  marker.color.a = 0.8; // Don't forget to set the alpha!
+  marker.color.r = 0.0;
+  marker.color.g = 1.0;
+  marker.color.b = 0.0;
+  marker.lifetime = ros::Duration();
+  g_collision_objs.markers.push_back(marker);
 }
